e63e594d677892592c4fd83a8758d49ea427955b
Danila Egorov <ya.ul2010@yandex.ru>
2022-04-19 20:58:19 +0400

    New GroupController and methods added

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function list(){
+       
+    }
+
+    public function create(){
+
+    }
+    
+    public function edit(){
+
+    }
+
+    public function delete(){
+        
+    }
+}
