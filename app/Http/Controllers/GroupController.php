bd4c83fb207773e56f6ec048caa68267ca91915c
Danila Egorov <ya.ul2010@yandex.ru>
2022-04-19 22:07:41 +0400

    Routes created

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,18 +7,18 @@ use Illuminate\Http\Request;
 class GroupController extends Controller
 {
     public function list(){
-       
+        return view('groups/show');
     }
 
     public function create(){
-
+        return view('groups/create');
     }
     
     public function edit(){
-
+        return view('groups/form');
     }
 
     public function delete(){
-        
+        return view('groups/show');
     }
 }
